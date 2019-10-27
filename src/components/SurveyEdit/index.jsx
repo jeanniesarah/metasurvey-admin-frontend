e9ba913ccf4dfde136bff59181859bbcfef0de45
uyouthe <melvoloskov@ya.ru>
2019-10-27 12:36:13 +0300

    sections

--- src/components/SurveyEdit/index.jsx
+++ src/components/SurveyEdit/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { Input, Spin, Typography } from "antd";
 import { saveSurvey, getSurvey } from "../../lib/api";
+import styles from "./styles.module.css";
 
 const { Title } = Typography;
 const SurveyEdit = () => {
@@ -21,15 +22,20 @@ const SurveyEdit = () => {
 
   return (
     <>
-      <Title>Edit survey</Title>
+      <section className={styles.section}>
+        <Title>Edit survey</Title>
 
-      <Input
-        size="large"
-        placeholder="Title"
-        value={title}
-        onChange={e => setTitle(e.target.value)}
-        onBlur={() => saveSurvey({ id, title })}
-      />
+        <Input
+          size="large"
+          placeholder="Title"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          onBlur={() => saveSurvey({ id, title })}
+        />
+      </section>
+      <section className={styles.section}>
+        <Title>Stats</Title>
+      </section>
     </>
   );
 };
