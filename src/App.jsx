e03b74399e7db89a1bca6038cb6ec75aaddf157b
uyouthe <melvoloskov@ya.ru>
2019-10-27 10:32:33 +0300

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import React from 'react';
+import React from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
+  BrowserRouter as Router,
+  Switch,
+  Route
+  // Link
 } from "react-router-dom";
-import './App.css';
-import SignUp from './components/SignUp';
-import Login from './components/Login';
-import SurveysList from './components/SurveysList';
-import SurveyEdit from './components/SurveyEdit';
+import "./App.css";
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import SurveysList from "./components/SurveysList";
+import SurveyEdit from "./components/SurveyEdit";
 
 function App() {
   return (
-      <Router>
-          <h1>MetaSurvey</h1>
-          <Switch>
-              <Route path="/signup">
-                  <SignUp />
-              </Route>
-              <Route path="/login">
-                  <Login />
-              </Route>
-              <Route path="/admin/surveys">
-                  <SurveysList />
-              </Route>
-              <Route path="/admin/survey/:id">
-                  <SurveyEdit />
-              </Route>
-          </Switch>
-      </Router>
+    <Router>
+      <h1>MetaSurvey</h1>
+      <Switch>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/admin/surveys">
+          <SurveysList />
+        </Route>
+        <Route path="/admin/survey/:id">
+          <SurveyEdit />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
