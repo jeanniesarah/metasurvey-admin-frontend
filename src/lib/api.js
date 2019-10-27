2caf18a42d74379a95bb52eac8cf97ccdcf05db8
Alex Belozerov <asbelozerov@gmail.com>
2019-10-27 16:42:59 +0300

    API integration

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,40 +1,75 @@
+const apiUrl = 'https://meta-survey-app.herokuapp.com/api';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE4MDQxNjQyNDUsImlkIjoiNWRiNDllZDg4ZTY0MmIxZDk1OWUxY2MyIiwiaWF0IjoxNTcyMTgwMjQ1fQ.GBa09_h-Uf5GIq288b5PfVdBqWPFUN-b2_aBT16LTkQ';
+
 export const getListOfSurveys = () => {
 	return {};
 };
 
 export const getSurvey = (id) => {
-	return Promise.resolve({
-		title: 'Dear user, why did you leaved us?',
-		showTextarea: true,
-		questions: [
-			{
-				id: 'some_id1',
-				text: 'Your UI is shit'
-			},
-			{
-				id: 'some_id_2',
-				text: 'Don\'t work for me at all',
-			}
-		]
-	});
+	return fetch(`${apiUrl}/admin/survey/${id}`, {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	})
+		.then(body => body.json());
+
 };
 
 export const saveSurvey = ({ surveyId, title }) => {
-	console.log('Survey saved to server', surveyId, title);
-	return Promise.resolve();
+	return fetch(`${apiUrl}/admin/survey/${surveyId}`, {
+		method: 'PATCH',
+		headers: {
+			Authorization: `Bearer ${token}`,
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ title })
+	})
+		.then(body => {
+			// console.log('Survey saved to server', surveyId, title);
+		});
 };
 
-export const addSurveyQuestion = ({ surveyId, questionId, text }) => {
-	console.log('Question added to server', surveyId, questionId, text);
-	return Promise.resolve({ id: 'new_id', text });
+export const addSurveyQuestion = ({ surveyId, text }) => {
+	return fetch(`${apiUrl}/admin/survey/${surveyId}/question`, {
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${token}`,
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ text })
+	})
+		.then(body => body.json())
+		.then(bodyJson => {
+			const addedQuestionId = bodyJson._id;
+			// console.log('Question added to server', surveyId, text, addedQuestionId);
+			return { ...bodyJson, id: addedQuestionId };
+		});
 };
 
 export const updateSurveyQuestion = ({ surveyId, questionId, text }) => {
-	console.log('Question saved to server', surveyId, questionId, text);
-	return Promise.resolve();
+	return fetch(`${apiUrl}/admin/survey/${surveyId}/question/${questionId}`, {
+		method: 'PATCH',
+		headers: {
+			Authorization: `Bearer ${token}`,
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ text })
+	})
+		.then(body => {
+			// console.log('Question saved to server', surveyId, questionId, text);
+			return body.json()
+		});
 };
 
-export const deleteSurveyQuestion = ({ questionId }) => {
-	console.log('Question deleted from server', questionId);
-	return Promise.resolve();
+export const deleteSurveyQuestion = ({ surveyId, questionId }) => {
+	return fetch(`${apiUrl}/admin/survey/${surveyId}/question/${questionId}`, {
+		method: 'DELETE',
+		headers: {
+			Authorization: `Bearer ${token}`
+		},
+	})
+		.then(body => {
+			// console.log('Question deleted from server', questionId);
+			return body.json()
+		});
 };
