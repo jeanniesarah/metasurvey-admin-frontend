347564fd129a4f2559175e120352f1ad16d86251
uyouthe <melvoloskov@ya.ru>
2019-10-27 12:07:25 +0300

    craete fild

--- src/components/SurveysList/index.jsx
+++ src/components/SurveysList/index.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { Typography } from "antd";
+import { Typography, Input } from "antd";
 
 import logo from "./logo.png";
 import styles from "./styles.module.css";
 
 const { Title } = Typography;
+const { Search } = Input;
 
 const SurveysList = () => {
   return (
     <div className={styles.root}>
       <img className={styles.logo} src={logo} alt="MetaSurvey" />
       <Title>Create survey</Title>
+      <Search
+        placeholder="meta-octopus"
+        enterButton="Create"
+        size="large"
+        onSearch={value => console.log(value)}
+      />
     </div>
   );
 };
