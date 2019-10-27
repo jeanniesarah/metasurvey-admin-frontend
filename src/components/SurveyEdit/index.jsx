e590e3c3cfc74b554a870f6d881c344bbac5bb05
Alex Belozerov <asbelozerov@gmail.com>
2019-10-27 12:17:28 +0300

    SurveyEdit with title, api mock

--- src/components/SurveyEdit/index.jsx
+++ src/components/SurveyEdit/index.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Input, Spin } from 'antd';
+import { saveSurvey, getSurvey } from '../../lib/api';
 
 const SurveyEdit = () => {
 	let { id } = useParams();
 
+	const [title, setTitle] = useState('');
+	const [survey, setSurvey] = useState(undefined);
+
+	if (!survey) {
+		getSurvey(id)
+			.then(survey => {
+				setSurvey(survey);
+				setTitle(survey.title);
+			});
+
+		return <Spin size="large" />
+	}
+
+
 	return <>
 		<h2>Survey Edit</h2>
-		<div>{ id }</div>
+
+		<Input size="large"
+			   placeholder="Title"
+			   value={title}
+			   onChange={(e) => setTitle(e.target.value)}
+			   onBlur={() => saveSurvey({ id, title })}
+		/>
 	</>;
 };
 
--- src/lib/api.js
+++ src/lib/api.js
@@ -3,9 +3,30 @@ export const getListOfSurveys = () => {
 };
 
 export const getSurvey = (id) => {
-	return {};
+	return Promise.resolve({
+		title: 'Dear user, why did you leaved us?',
+		showTextarea: true,
+		questions: [
+			{
+				id: 'some_id1',
+				text: 'Your UI is shit'
+			},
+			{
+				id: 'some_id_2',
+				text: 'Don\'t work for me at all',
+			}
+		]
+	});
 };
 
-export const saveSurvey = (payload) => {
+export const saveSurvey = ({ title }) => {
+	console.log('Survey saved to server', title);
+};
 
+export const addSurveyQuestion = ({ text }) => {
+	console.log('Question added to server', text);
+};
+
+export const updateSurveyQuestion = ({ id, text }) => {
+	console.log('Question saved to server', id, text);
 };
