f97cc38df3cd406c8c6d7e685a4f2961f178af1c
uyouthe <melvoloskov@ya.ru>
2019-10-27 12:12:05 +0300

    basic page

--- src/components/SurveysList/index.jsx
+++ src/components/SurveysList/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, Input } from "antd";
+import { Typography, Input, Button } from "antd";
 
 import logo from "./logo.png";
 import styles from "./styles.module.css";
@@ -11,13 +11,27 @@ const SurveysList = () => {
   return (
     <div className={styles.root}>
       <img className={styles.logo} src={logo} alt="MetaSurvey" />
-      <Title>Create survey</Title>
-      <Search
-        placeholder="meta-octopus"
-        enterButton="Create"
-        size="large"
-        onSearch={value => console.log(value)}
-      />
+      <section className={styles.section}>
+        <Title>Create survey</Title>
+        <Search
+          placeholder="meta-octopus"
+          enterButton="Create"
+          size="large"
+          onSearch={value => console.log(value)}
+        />
+      </section>
+      <section className={styles.section}>
+        <Title>Your surveys</Title>
+
+        <div className={styles.surveys}>
+          <Button type="primary" shape="round" size="large">
+            meta-octopus
+          </Button>
+          <Button type="primary" shape="round" size="large">
+            super-chipmunk
+          </Button>
+        </div>
+      </section>
     </div>
   );
 };
