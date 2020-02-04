3d0e494b47569d596dc09f5f9a4446ea6f6c55fb
Alex Belozerov <asbelozerov@gmail.com>
2020-02-05 00:11:44 +0300

    2019 -> 2020

--- src/admin/components/FooterContent/index.jsx
+++ src/admin/components/FooterContent/index.jsx
@@ -3,25 +3,40 @@ import { Button, Menu } from 'antd';
 import styles from './styles.module.css';
 
 const FooterContent = () => {
-	return <>
-		<div style={{float: 'left', height: 32}}>
-			<span style={{ lineHeight: '32px' }}>Created by the <a href={'https://www.indiehackers.com/product/metasurvey'} target="_blank">MetaSurvey</a> Team 2019</span>
-		</div>
-		<div style={{float: 'right'}}>
-			<Button
-				type={'link'}
-				href={'mailto:indiehackersspb@gmail.com'}
-				className={styles.buttonHoverHack}
-				style={{ color: '#6A6A6A' }}
-				>Support</Button>
-			<Button
-				type={'link'}
-				href={'mailto:indiehackersspb@gmail.com'}
-				className={styles.buttonHoverHack}
-				style={{ color: '#FF3399' }}
-				>Report bug</Button>
-		</div>
-	</>
+  return (
+    <>
+      <div style={{ float: 'left', height: 32 }}>
+        <span style={{ lineHeight: '32px' }}>
+          Created by the{' '}
+          <a
+            href={'https://www.indiehackers.com/product/metasurvey'}
+            target="_blank"
+          >
+            MetaSurvey
+          </a>{' '}
+          Team 2020
+        </span>
+      </div>
+      <div style={{ float: 'right' }}>
+        <Button
+          type={'link'}
+          href={'mailto:indiehackersspb@gmail.com'}
+          className={styles.buttonHoverHack}
+          style={{ color: '#6A6A6A' }}
+        >
+          Support
+        </Button>
+        <Button
+          type={'link'}
+          href={'mailto:indiehackersspb@gmail.com'}
+          className={styles.buttonHoverHack}
+          style={{ color: '#FF3399' }}
+        >
+          Report bug
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default FooterContent;
