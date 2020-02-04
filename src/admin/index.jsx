70ec08497296c4b77c67961ad0b1f8ad399590ac
Alex Belozerov <asbelozerov@gmail.com>
2020-02-05 00:09:15 +0300

    Footer is on bottom of the screen when not enough content

--- src/admin/index.jsx
+++ src/admin/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
+  BrowserRouter as Router,
+  Switch,
+  Route,
 } from 'react-router-dom';
 import { Layout, Menu, Breadcrumb } from 'antd';
 import HeaderContent from './components/HeaderContent';
@@ -16,22 +16,24 @@ import styles from './styles.module.css';
 const { Header, Content, Footer } = Layout;
 
 function Admin() {
-	return <Layout>
-		<Header className={styles.header}>
-			<HeaderContent/>
-		</Header>
-		<Content className={styles.content}>
-			<Route path="/admin/surveys">
-				<SurveysList />
-			</Route>
-			<Route path="/admin/survey/:id">
-				<SurveyEdit />
-			</Route>
-		</Content>
-		<Footer className={styles.footer}>
-			<FooterContent/>
-		</Footer>
-	</Layout>
+  return (
+    <Layout className={styles.layout}>
+      <Header className={styles.header}>
+        <HeaderContent />
+      </Header>
+      <Content className={styles.content}>
+        <Route path="/admin/surveys">
+          <SurveysList />
+        </Route>
+        <Route path="/admin/survey/:id">
+          <SurveyEdit />
+        </Route>
+      </Content>
+      <Footer className={styles.footer}>
+        <FooterContent />
+      </Footer>
+    </Layout>
+  );
 }
 
 export default Admin;
